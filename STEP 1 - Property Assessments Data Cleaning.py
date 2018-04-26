4fbab3871eab5557d9f8ae51a88839e965dea4bc
Nick Kharas <nkharas@andrew.cmu.edu>
2018-04-26 02:31:28 -0400

    Changes in data cleaning

--- STEP 1 - Property Assessments Data Cleaning.py
+++ STEP 1 - Property Assessments Data Cleaning.py
@@ -1,23 +1,26 @@
 # Author: Max Haytovich
-# Changes and comments by Nick Kharas
+# Changes: Nick Kharas
+
 # Import packages
 import pandas as pd
 import re
 
-# Note: The callable API does not return all the data as a chunk. The URL downloads the data as of Dec 2016.
+# Note: The URL downloads the data as of Mar 2018.
 # For data as of other time periods, please refer to the WPRDC data set https://data.wprdc.org/dataset/property-assessments
-# The download is broken into separate chunks and concatenated again, for below reasons.
-# 1. Pandas allocates the data type of a field based on initial values. This will break if the initial values of an alphanumeric column are numbers
-# 2. Another workaround for the problem in (1) is to import the file in a single chunk. However, I did not have enough memory for that brute force approach
-propAssessment = pd.read_csv('https://data.wprdc.org/dataset/2b3df818-601e-4f06-b150-643557229491/resource/7ef45883-68d6-48ee-a466-e625e31809f6/download/alleghenycountymasterfile01022017.csv', low_memory=False, iterator = True, chunksize = 50000)
+# The download is broken into separate chunks and concatenated again, as:
+# 1. Pandas allocates the data type of a field based on initial values. This will break if the initial values in an alphanumeric column are numbers
+# 2. Another workaround for the problem is to import the file as a single chunk. However, I did not have enough memory for that brute force approach
+propAssessment = pd.read_csv('https://data.wprdc.org/dataset/2b3df818-601e-4f06-b150-643557229491/resource/f2b8d575-e256-4718-94ad-1e12239ddb92/download/assessments.csv', low_memory=False, iterator = True, chunksize = 50000)
 propAssessment = pd.DataFrame(pd.concat(propAssessment))
 
-# Since we are evaluating the housing problem in Pitsburgh, we only want to analyze valid sales of residential properties
-propAssessment = propAssessment[(( (propAssessment['SALEDESC'] == 'VALID SALE') | (propAssessment['SALEDESC'] == 'OTHER VALID') ) & (propAssessment['CLASSDESC'] == 'RESIDENTIAL'))]
+# Since we want to identify genuine housing problems, we only analyze residential properties with relevant sale codes
+# This step is important to ultimately identify and segment houses that are in poor condition
+# Definition of sale codes - https://data.wprdc.org/dataset/9e0ce87d-07b8-420c-a8aa-9de6104f61d6/resource/96474373-bcdb-42cf-af5d-3683e326e227/download/sales-validation-codes-dictionary.pdf
+propAssessment = propAssessment[(propAssessment['SALECODE'].isin(['0','U','UR','14','16','33','34','36','99','BK'])  ) & (propAssessment['CLASSDESC'] == 'RESIDENTIAL') ]
 non_decimal = re.compile(r'[^\d.]+')
 
-# Added by Nick
-# Null filling - Only values that are flagged are given in the file, rest are null. For example, HOMESTEADFLAG = "HOM" if true, NULL otherwise. This is fixed
+# Handle missing values where obvious
+# for the below three columns, only the "yes" values are filled. The rest are null. For example, HOMESTEADFLAG = "HOM" if true, NULL otherwise. This is fixed
 propAssessment.loc[(propAssessment.HOMESTEADFLAG != 'HOM') | (pd.isnull(propAssessment['HOMESTEADFLAG'])),'HOMESTEADFLAG'] = 'None'
 propAssessment.loc[(propAssessment.FARMSTEADFLAG != 'FRM') | (pd.isnull(propAssessment['FARMSTEADFLAG'])),'FARMSTEADFLAG'] = 'None'
 propAssessment.loc[(propAssessment.CLEANGREEN != 'Y') | (pd.isnull(propAssessment['CLEANGREEN'])),'CLEANGREEN'] = 'N'
@@ -28,34 +31,23 @@ propAssessment['PriceDiff1_per'] = (((propAssessment.SALEPRICE - propAssessment.
 propAssessment['PriceDiff2'] = propAssessment.PREVSALEPRICE - propAssessment.PREVSALEPRICE2
 propAssessment['PriceDiff2_per'] = (((propAssessment.PREVSALEPRICE - propAssessment.PREVSALEPRICE2) * 1.0) / (propAssessment.PREVSALEPRICE * 1.0)) * 100.0
 
-# Find he ownership duraton before the property was last sold
+# Find the ownership duraton before the property was last sold
 propAssessment['SALEDATE'] = pd.to_datetime(propAssessment['SALEDATE'], errors='coerce')
 propAssessment['PREVSALEDATE'] = pd.to_datetime(propAssessment['PREVSALEDATE'], errors='coerce')
 propAssessment['PREVSALEDATE2'] = pd.to_datetime(propAssessment['PREVSALEDATE2'], errors='coerce')
 propAssessment['DateDiff1'] = propAssessment['SALEDATE'] - propAssessment['PREVSALEDATE']
 propAssessment['DateDiff2'] = propAssessment['PREVSALEDATE'] - propAssessment['PREVSALEDATE2']
 
-# Added by Nick
-# There is missing data on Previous Sale Date
-# Some transactions have the previous sale date coming after the most recent sale date, leading to negative ownership duration
-# These are considered as errors and ignored, 45 out of 180000 records
-propAssessment = propAssessment[(propAssessment['DateDiff1'] >= pd.Timedelta('0 days')) | (pd.isnull(propAssessment['DateDiff1']))]
-
-# Use the logic given in the property assesments data dictionary to highlight vacant properties
+# Use the logic given in the property assesments data dictionary by the WPRDC (link below) to highlight vacant properties
+# https://data.wprdc.org/dataset/2b3df818-601e-4f06-b150-643557229491/resource/cc4bafd2-25b6-41d7-83aa-d16bc211b020/download/alleghenycountypropertyassessmentdatauserguide-4.pdf
 propAssessment['isVacant'] = 0
 propAssessment.loc[(propAssessment.FAIRMARKETBUILDING == 0) ,'isVacant'] = 1
 
-###### This is the output class label
-# This line of code was updated to use the results of clustering in STEP 2 to determine class boundaries, and flag the class labels for training and testing predctive models.
-propAssessment['ClassLabel'] = 0
-propAssessment.loc[(propAssessment.DateDiff1 < pd.Timedelta('1100 days')) & (propAssessment.DateDiff1 >= pd.Timedelta('0 days')) & ((propAssessment.PriceDiff1_per < 15) | (propAssessment.SALEPRICE < 90000)),'ClassLabel'] = 1
-
-# Added by Nick
-# Output file for classification modeling
-propAssessment[['PROPERTYZIP','MUNIDESC','SCHOOLDESC','NEIGHDESC','TAXDESC','OWNERDESC','USEDESC','LOTAREA','HOMESTEADFLAG','FARMSTEADFLAG','CLEANGREEN','SALEPRICE','COUNTYBUILDING','COUNTYLAND','COUNTYTOTAL','COUNTYEXEMPTBLDG','LOCALBUILDING','LOCALLAND','LOCALTOTAL','FAIRMARKETBUILDING','FAIRMARKETLAND','FAIRMARKETTOTAL','STYLEDESC','STORIES','YEARBLT','EXTFINISH_DESC','ROOFDESC','BASEMENTDESC','GRADE','CONDITIONDESC','CDU','TOTALROOMS','BEDROOMS','FULLBATHS','HALFBATHS','HEATINGCOOLINGDESC','FIREPLACES','BSMTGARAGE','FINISHEDLIVINGAREA','isVacant','ClassLabel']].to_csv('property_assessments_clean.csv', index = False, index_label = False)
+# Output file for modeling with classification algorithms
+propAssessment[['PARID','PROPERTYZIP','MUNIDESC','SCHOOLDESC','NEIGHDESC','TAXDESC','OWNERDESC','USEDESC','LOTAREA','HOMESTEADFLAG','FARMSTEADFLAG','CLEANGREEN','SALEPRICE','SALECODE','COUNTYBUILDING','COUNTYLAND','COUNTYEXEMPTBLDG','LOCALBUILDING','LOCALLAND','FAIRMARKETBUILDING','FAIRMARKETLAND','STYLEDESC','STORIES','YEARBLT','EXTFINISH_DESC','ROOFDESC','BASEMENTDESC','GRADE','CONDITIONDESC','CDU','TOTALROOMS','BEDROOMS','FULLBATHS','HALFBATHS','HEATINGCOOLINGDESC','FIREPLACES','BSMTGARAGE','FINISHEDLIVINGAREA','isVacant']].to_csv('property_assessments_clean.csv', index = False, index_label = False)
 
 # Output file for clustering
-price_date_diff = propAssessment[['PriceDiff1_per','DateDiff1','SALEPRICE']][propAssessment['PriceDiff1_per'].notnull()] # Get price and date differences where price is not null
+price_date_diff = propAssessment[['PARID','PriceDiff1_per','DateDiff1','SALEPRICE']][propAssessment['PriceDiff1_per'].notnull()] # Get price and date differences where price is not null
 price_date_diff[price_date_diff['DateDiff1'].notnull()].to_csv('price_date_diff.csv', index = False, index_label = False)
 
 # Output file for hypothesis test
